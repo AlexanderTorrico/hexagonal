6490799d2bbdecef9b7dec7486887ba2fc1c8f3a
AlexanderTorrico <torrico.torrico.alexander@gmail.com>
2025-12-01 00:17:18 -0400

    excluir

--- src/main/java/com/example/hexagonal/HexagonalApplication.java
+++ src/main/java/com/example/hexagonal/HexagonalApplication.java
@@ -3,7 +3,7 @@ package com.example.hexagonal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class})
 public class HexagonalApplication {
 
 	public static void main(String [] args) {
